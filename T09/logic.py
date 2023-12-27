caffa626e91554320f6415ad86589cff4c50b65d
tolumorohunfola <111201704+tolumorohunfola@users.noreply.github.com>
2023-12-27 18:46:19 +0000

    Created logic file and displays logic error MVP

---
+++ T09/logic.py
@@ -0,0 +1,15 @@
+# A program to display a logical error
+
+print("This is a program to display a logic error: ")
+
+print("This is proof that since we are in 2023, in 77 years, the year will be 3000!")
+
+# assigning the current year
+current_year = 2023
+
+#assigning the years added to the current year
+years_added = 977 # Location of logic error, instead of typing 77 years, typed 977 years! big difference
+
+future_year = current_year + years_added
+
+print(f"The current year is {current_year}, in {years_added} years, the year will be: {future_year} :0")
